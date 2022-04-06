ab578c122e84e3700dd6870355cc04ddfe352631
Anand <reaching.anandsingh@gmail.com>
2022-04-06 22:18:58 +0530

    Added Entities

---
+++ AirlineReservationSystem/Entities/AirlineDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirlineReservationSystem.Entities
+{
+    public class AirLineDbContext : DbContext
+    {
+        //Entity Sets
+        public DbSet<User> Users { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Flight> Flights { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=MSI\SQLEXPRESS; Database=AirLineDB; Integrated Security=true");
+        }
+    }
+}
---
+++ AirlineReservationSystem/Entities/Flights.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AirlineReservationSystem.Entities
+{
+
+    [Table("Flights")]
+    public class Flight
+    {
+        //FlightId 
+        [Key]
+        [Column(TypeName = "int")]
+        public int FlightID { get; set; }
+
+        // Date when the Flight was added to the fleet
+        [Required]
+        [Column(TypeName = "Date")]
+        public DateTime LaunchDate { get; set; }
+
+        //SOurce Airport
+        [Required]
+        [StringLength(10)]
+        [Column(TypeName = "varchar")]
+        public string Origin { get; set; }
+
+
+        //destination airport
+        [Required]
+        [StringLength(10)]
+        [Column(TypeName = "varchar")]
+        public string Destination { get; set; }
+
+
+        //departure time of flight from source
+        [Required]
+        [Column(TypeName = "Date")]
+        public DateTime DeptTime { get; set; }
+
+        //arrival time of flight at destination
+        [Required]
+        [Column(TypeName = "Date")]
+        public DateTime ArrivalTime { get; set; }
+
+        //no of seats passenger wants to book
+        [Required]
+        [Column(TypeName = "int")]
+        public int NoOfSeats { get; set; }
+
+        //cost of a trip
+        [Required]
+        [Column(TypeName = "decimal")]
+        public float Fare { get; set; }
+
+    }
+}
